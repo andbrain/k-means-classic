751c677a9c5fa990813357a6a268c70a4fce54cf
Anderson Pimentel <anderson.santos@itriad.org.br>
2017-09-12 17:38:12 -0400

    feat: add main structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # k-means-classic
 Classic k-means using euclidean distance in C++
+
+## Author
+@andbrain -> andbrain@gmail.com
---
+++ kmeans.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "mat.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "Ola" << endl;
+	return 0;
+}
